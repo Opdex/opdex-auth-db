ee3863e55d5fd146eafa4cd2f46af53006fb6f58
Tyler Pena <tylerpena@me.com>
2022-03-24 16:24:59 -0700

    add temp admin table back

--- pipeline-scripts/create-database.sql
+++ pipeline-scripts/create-database.sql
@@ -6,18 +6,30 @@ DROP PROCEDURE IF EXISTS CreateDatabase;
 
 CREATE PROCEDURE CreateDatabase ()
     BEGIN
+        CREATE TABLE IF NOT EXISTS admin
+        (
+            Id      BIGINT UNSIGNED AUTO_INCREMENT,
+            Address VARCHAR(50) NOT NULL,
+            PRIMARY KEY (Id),
+            UNIQUE admin_address_uq (Address)
+        ) ENGINE=INNODB;
+
         CREATE TABLE IF NOT EXISTS auth_success(
             ConnectionId       VARCHAR(255) NOT NULL,
             Signer             VARCHAR(50) NOT NULL,
             Expiry             DATETIME NOT NULL,
-            PRIMARY KEY (ConnectionId)
+            PRIMARY KEY (ConnectionId),
+            INDEX auth_success_connection_id_ix (ConnectionId),
+            INDEX auth_success_signer_ix (Signer)
         ) ENGINE = INNODB;
 
         CREATE TABLE IF NOT EXISTS auth_access_code(
             AccessCode         VARCHAR(50) NOT NULL,
             Signer             VARCHAR(50) NOT NULL,
             Expiry             DATETIME NOT NULL,
-            PRIMARY KEY (AccessCode)
+            PRIMARY KEY (AccessCode),
+            INDEX auth_access_code_access_code_ix (AccessCode),
+            INDEX auth_access_code_signer_ix (Signer)
         ) ENGINE = INNODB;
     END;
 //
