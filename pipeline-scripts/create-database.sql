f5b851ca1192373d6aa15a52cb71dc303831f94e
drmathias <adam@opdex.com>
2022-04-01 14:31:09 +0100

    [AAPI-412] Amend tables for PKCE flow

--- pipeline-scripts/create-database.sql
+++ pipeline-scripts/create-database.sql
@@ -6,24 +6,47 @@ DROP PROCEDURE IF EXISTS CreateDatabase;
 
 CREATE PROCEDURE CreateDatabase ()
     BEGIN
-        CREATE TABLE IF NOT EXISTS auth_success(
-            ConnectionId       VARCHAR(255) NOT NULL,
-            Signer             VARCHAR(50) NOT NULL,
-            Expiry             DATETIME NOT NULL,
-            PRIMARY KEY (ConnectionId),
-            INDEX auth_success_connection_id_ix (ConnectionId),
-            INDEX auth_success_signer_ix (Signer)
+        CREATE TABLE IF NOT EXISTS code_challenge_method(
+            Id      SMALLINT UNSIGNED AUTO_INCREMENT,
+            Name    VARCHAR(5) NOT NULL,
+            PRIMARY KEY (Id),
+            UNIQUE code_challenge_method_name_uq (Name)
         ) ENGINE = INNODB;
 
-        CREATE TABLE IF NOT EXISTS auth_access_code(
-            AccessCode         VARCHAR(50) NOT NULL,
+        CREATE TABLE IF NOT EXISTS auth_session(
+            Id                      BINARY(16) NOT NULL,
+            CodeChallenge           VARCHAR(255) NOT NULL,
+            CodeChallengeMethod     SMALLINT UNSIGNED NOT NULL,
+            ConnectionId            VARCHAR(255) NULL,
+            PRIMARY KEY (Id),
+            UNIQUE auth_session_code_challenge_uq (CodeChallenge),
+            UNIQUE auth_session_connection_id_uq (ConnectionId),
+            CONSTRAINT auth_session_code_challenge_method_id_fk
+                FOREIGN KEY (CodeChallengeMethod)
+                REFERENCES code_challenge_method (Id)
+        ) ENGINE = INNODB;
+
+        CREATE TABLE auth_access_code(
+            AccessCode         BINARY(16) NOT NULL,
             Signer             VARCHAR(50) NOT NULL,
+            Stamp              BINARY(16) NOT NULL,
             Expiry             DATETIME NOT NULL,
             PRIMARY KEY (AccessCode),
-            INDEX auth_access_code_access_code_ix (AccessCode),
-            INDEX auth_access_code_signer_ix (Signer)
+            UNIQUE auth_access_code_stamp_uq (Stamp),
+            INDEX auth_access_code_signer_ix (Signer),
+            CONSTRAINT auth_access_code_stamp_fk
+                FOREIGN KEY (Stamp)
+                REFERENCES auth_session (Id) ON DELETE CASCADE
         ) ENGINE = INNODB;
     END;
+
+    -- --------
+    -- --------
+    -- Populate Lookup Type Tables
+    -- -------
+    -- -------
+    INSERT INTO code_challenge_method(Name)
+        VALUES ('Plain'), ('S256');
 //
 
 CALL CreateDatabase();
@@ -37,13 +60,6 @@ DROP PROCEDURE CreateDatabase;
 -- Events
 -- --------
 -- --------
-
-DROP EVENT IF EXISTS RemoveExpiredAuthSuccessEvent;
-
-CREATE EVENT IF NOT EXISTS RemoveExpiredAuthSuccessEvent
-ON SCHEDULE EVERY 5 MINUTE
-DO DELETE FROM auth_success WHERE Expiry < UTC_TIMESTAMP();
-//
 
 DROP EVENT IF EXISTS RemoveExpiredAuthAccessCodeEvent;
 
